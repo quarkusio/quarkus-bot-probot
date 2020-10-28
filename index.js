2afc0a6895d2a426bb4cd8347ad17830a2ac39e3
Guillaume Smet <guillaume.smet@gmail.com>
2020-10-28 13:04:26 +0100

    Add the first set of editorial rules for pull requests

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
+const checkPullRequestEditorialRules = require('./lib/check-pull-request-editorial-rules')
+
 module.exports = app => {
-  // Your code here
-  app.log.info('Yay, the app was loaded!')
+  app.log.info('quarkus-bot running...')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  app.on(['pull_request.opened', 'pull_request.edited'], checkPullRequestEditorialRules)
 }
