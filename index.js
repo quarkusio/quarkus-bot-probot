d02433c43723420bd602574b86cb29ee13e536fc
Guillaume Smet <guillaume.smet@gmail.com>
2020-10-28 14:41:21 +0100

    First version of automatic milestone affectation

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const affectMilestone = require('./lib/affect-milestone')
 const checkPullRequestEditorialRules = require('./lib/check-pull-request-editorial-rules')
 
 module.exports = app => {
   app.log.info('quarkus-bot running...')
 
   app.on(['pull_request.opened', 'pull_request.edited'], checkPullRequestEditorialRules)
+  app.on(['pull_request.closed'], affectMilestone)
 }
